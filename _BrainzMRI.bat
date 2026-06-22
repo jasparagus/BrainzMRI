d5d0f2a50d6ab577d2ddf0e1ee52260260d11785
jasparagus <jasperthecook@gmail.com>
2026-06-22 08:11:05 -0700

    clean repo

--- _BrainzMRI.bat
+++ _BrainzMRI.bat
@@ -5,27 +5,7 @@ echo ============================================
 echo   BrainzMRI Launcher
 echo ============================================
 echo.
-echo   1. Run BrainzMRI GUI
-echo   2. Debug
-echo.
-echo   (Default = GUI, opening in 1 second...)
-echo.
 
-choice /C 12 /N /T 1 /D 1 >nul
-
-if errorlevel 2 goto DEBUG
-if errorlevel 1 goto GUI
-
-:DEBUG
-echo Starting ParseListens DEBUG...
-python parsing.py
-goto end
-
-:GUI
-echo Starting BrainzMRI GUI...
 python gui_main.py
-goto end
 
-
-:end
 pause
